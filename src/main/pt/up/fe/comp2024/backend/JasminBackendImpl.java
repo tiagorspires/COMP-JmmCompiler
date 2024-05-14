61719a177c19ecd7d8cb3d610b9c5cfc826a7cfb
tiago <up202008790@up.pt>
2024-05-14 11:04:25 +0100

    Revert "test"
    This reverts commit 43eff715f9cc4c506e005a041cf4e7e1b2fea0ee.

--- src/main/pt/up/fe/comp2024/backend/JasminBackendImpl.java
+++ src/main/pt/up/fe/comp2024/backend/JasminBackendImpl.java
@@ -3,57 +3,16 @@ package pt.up.fe.comp2024.backend;
 import pt.up.fe.comp.jmm.jasmin.JasminBackend;
 import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
-import pt.up.fe.comp.jmm.report.Report;
-
-import java.util.ArrayList;
 
 public class JasminBackendImpl implements JasminBackend {
 
     @Override
     public JasminResult toJasmin(OllirResult ollirResult) {
 
+        var jasminGenerator = new JasminGenerator(ollirResult);
+        var jasminCode = jasminGenerator.build();
 
-
-        return new JasminResult(ollirResult, ".class public Method\n" +
-                ".super java/lang/Object\n" +
-                "\n" +
-                ".method public static main([Ljava/lang/String;)V\n" +
-                "\n" +
-                "\t.limit stack 4\n" +
-                "\t.limit locals 6\n" +
-                "\tbipush 10\n" +
-                "\tnewarray int\n" +
-                "\tastore_1\n" +
-                "\ticonst_2\n" +
-                "\tistore_2\n" +
-                "\ticonst_1\n" +
-                "\tistore_3\n" +
-                "\tnew Method\n" +
-                "\tastore 4\n" +
-                "\taload 4\n" +
-                "\tinvokespecial Method/<init>()V\n" +
-                "\taload 4\n" +
-                "\tiload_3\n" +
-                "\tiload_2\n" +
-                "\taload_1\n" +
-                "\tinvokevirtual Method/foo(ZI[I)I\n" +
-                "\tistore 5\n" +
-                "\treturn\n" +
-                ".end method\n" +
-                "\n" +
-                ".method public foo(ZI[I)I\n" +
-                "\n" +
-                "\t.limit stack 1\n" +
-                "\t.limit locals 4\n" +
-                "\tiload_2\n" +
-                "\tireturn\n" +
-                ".end method\n" +
-                "\n" +
-                ".method public <init>()V\n" +
-                "\taload_0\n" +
-                "\tinvokespecial java/lang/Object/<init>()V\n" +
-                "\treturn\n" +
-                ".end method", new ArrayList<Report>());
+        return new JasminResult(ollirResult, jasminCode, jasminGenerator.getReports());
     }
 
 }
--- src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
+++ src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
@@ -11,33 +11,10 @@ public class JmmOptimizationImpl implements JmmOptimization {
     @Override
     public OllirResult toOllir(JmmSemanticsResult semanticsResult) {
 
+        var visitor = new OllirGeneratorVisitor(semanticsResult.getSymbolTable());
+        var ollirCode = visitor.visit(semanticsResult.getRootNode());
 
-
-        return new OllirResult(semanticsResult, "myClass {\n" +
-                "    .construct myClass().V {\n" +
-                "   \t invokespecial(this, \"<init>\").V;\n" +
-                "    }\n" +
-                "    \n" +
-                "    .method public sum(A.array.i32, B.array.i32).array.i32 {\n" +
-                "   \t t1.i32 :=.i32 arraylength($1.A.array.i32).i32;\n" +
-                "   \t C.array.i32 :=.array.i32 new(array, t1.i32).array.i32;\n" +
-                "   \t i.i32 :=.i32 0.i32;\n" +
-                "   \t \n" +
-                "   \t Loop:\n" +
-                "   \t\t t1.i32 :=.i32 arraylength($1.A.array.i32).i32;\n" +
-                "   \t\t if (i.i32 >=.bool t1.i32) goto End;\n" +
-                "   \t\t \n" +
-                "   \t\t t2.i32 :=.i32 $1.A[i.i32].i32;\n" +
-                "   \t\t t3.i32 :=.i32 $2.B[i.i32].i32;\n" +
-                "   \t\t t4.i32 :=.i32 t2.i32 +.i32 t3.i32;\n" +
-                "   \t\t C[i.i32].i32 :=.i32 t4.i32;\n" +
-                "   \t\t i.i32 :=.i32 i.i32 +.i32 1.i32;\n" +
-                "   \t\t goto Loop;\n" +
-                "   \t End:\n" +
-                "   \t\t ret.array.i32 C.array.i32;\n" +
-                "    }\n" +
-                "}\n" +
-                "\n", Collections.emptyList());
+        return new OllirResult(semanticsResult, ollirCode, Collections.emptyList());
     }
 
     @Override
